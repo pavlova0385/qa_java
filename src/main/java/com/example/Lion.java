19f92f5339903ec9d0da4c2a63c521f056f20c72
Anna pavlova <pavlova0385@yandex.ru>
2026-05-19 21:50:37 +0300

    исправлено

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,19 +5,17 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    private final Predator predator;
-    private final int kittens;
+    private final Feline feline;
 
-    public Lion(String sex, Predator predator, int kittens) throws Exception {
-        this.predator = predator;
-        this.kittens = kittens;
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
 
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
 
@@ -26,10 +24,10 @@ public class Lion {
     }
 
     public int getKittens() {
-        return kittens;
+        return feline.getKittens();
     }
 
     public List<String> getFood() throws Exception {
-        return predator.eatMeat();
+        return feline.getFood("Хищник");
     }
 }
